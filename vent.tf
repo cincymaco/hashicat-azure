15c3a5f309361a3d5a382d4b10b8b0decc6600d7
TJ Maco <tj.maco@Kroger.com>
2020-08-26 19:00:19 +0000

    Updated vnet module

--- vent.tf
+++ vent.tf
@@ -1,6 +1,18 @@
+
+resource "azurerm_resource_group" "example" {
+  name     = "my-resources"
+  location = "West Europe"
+}
 module "vnet" {
   source  = "app.terraform.io/tjmaco-training/vnet/azurerm"
   version = "2.1.0"
-  resource_group_name = "my-resources"
-  # insert required variables here
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
+
+  tags = {
+    environment = "dev"
+    costcenter  = "it"
+  }
 }
