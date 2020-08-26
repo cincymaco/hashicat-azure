76f251ef8b6631f1d82904b89c3a5da4f83e019b
TJ Maco <tj.maco@Kroger.com>
2020-08-26 18:55:38 +0000

    Updated vnet module

--- vent.tf
+++ vent.tf
@@ -1,3 +1,9 @@
+
+resource "azurerm_resource_group" "example" {
+  name     = "my-resources"
+  location = "centralus"
+}
+
 module "vnet" {
   source  = "app.terraform.io/tjmaco-training/vnet/azurerm"
   version = "2.1.0"
